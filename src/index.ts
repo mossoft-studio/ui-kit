87ea38b1de2594c645207f96f7085d8fb65e429b
kamashina <sharif.kamran@mail.ru>
2024-01-15 22:24:47 +0300

    test styles

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
+import "./Button/button.module.css";
+import "./Loader/loader.module.css";
+import Button from "./Button/Button";
+import Icon from "./Icon/Icon";
+import Loader from "./Loader/Loader";
 
-
-import Button from './Button/Button';
-import Icon from './Icon/Icon';
-import Loader from './Loader/Loader';
-
-
-export { Button, Icon ,Loader };
+export { Button, Icon, Loader };
