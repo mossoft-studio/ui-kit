c01f206de0ed787ba197daca5dde16b2ab190676
kamashina <sharif.kamran@mail.ru>
2024-01-15 22:37:29 +0300

    add error text

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import "./Loader/loader.module.css";
 import Button from "./Button/Button";
 import Icon from "./Icon/Icon";
 import Loader from "./Loader/Loader";
+import Checkbox from "./Checkbox/Checkbox";
+import ErrorText from "./ErrorText/ErrorText";
 
-export { Button, Icon, Loader };
+export { Button, Icon, Loader, Checkbox, ErrorText };
