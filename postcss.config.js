fec26fbfc1e9bea9b5c7b82f99ff31bddc675161
kamashina <sharif.kamran@mail.ru>
2024-01-15 18:46:08 +0300

    test

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // Другие плагины, если необходимо
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
