7fb91cf9a3f0b22ad9d3cb31352ad970e2e16769
kamashina <sharif.kamran@mail.ru>
2024-01-15 22:25:06 +0300

    build fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
-import Button from './Button/Button';
-import Icon from './Icon/Icon';
-import Loader from './Loader/Loader';
+import "./Button/button.module.css";
+import "./Loader/loader.module.css";
+import Button from "./Button/Button";
+import Icon from "./Icon/Icon";
+import Loader from "./Loader/Loader";
 export { Button, Icon, Loader };
