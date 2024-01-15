1cac625594fa3e288081a27599a60c8a62b1d4d5
kamashina <sharif.kamran@mail.ru>
2024-01-15 22:22:06 +0300

    test styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("tailwindcss"),
+    require("tailwindcss")("./tailwind.config"),
     require("autoprefixer"),
-
   ]
 }
