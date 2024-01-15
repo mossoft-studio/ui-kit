09826ca1acd4d0727bd79cd188190eb040077ae9
kamashina <sharif.kamran@mail.ru>
2024-01-15 16:38:09 +0300

    change index ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 
-export { useTheme } from '../tailwind.config';
-export * from './Loader/Loader'
-export * from './Button/Button'
-export * from './Icon/Icon'
+import { useTheme } from '../tailwind.config';
+import Button from './Button/Button';
+import Icon from './Icon/Icon';
+import Loader from './Loader/Loader';
+
+
+export { Button, Icon ,Loader, useTheme };
