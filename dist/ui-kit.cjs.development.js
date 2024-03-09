7bce71775a7bf20639fd622677da5037a005ace7
kamashina <sharif.kamran@mail.ru>
2024-03-09 18:37:56 +0300

    hotfix hotfix

--- dist/ui-kit.cjs.development.js
+++ dist/ui-kit.cjs.development.js
@@ -149,7 +149,6 @@ const Select = ({
   return React__default.createElement("div", null, React__default.createElement("div", {
     className: `relative w-full min-w-[190px] after:absolute after:right-4 after:top-0 after:bottom-0 after:my-auto after:w-2 after:h-2 after:border-t-2 after:border-r-2 after:border-dark-gray after:rotate-[134deg] after:transition-all after:duration-300 ${parentClassName}`
   }, React__default.createElement("select", Object.assign({}, field, rest, {
-    defaultValue: "",
     className: `w-full cursor-pointer appearance-none bg-light-gray border-[1px] border-light-gray rounded-[30px] md:border-white md:rounded-[15px] text-dark-gray text-sm md:text-base font-normal placeholder:text-dark-gray block py-[6px] px-[10px] !pr-9 md:py-[7px] transition-all duration-300 ${className}`
   }), React__default.createElement("option", {
     value: "",
