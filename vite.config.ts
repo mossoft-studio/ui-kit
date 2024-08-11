9817f893c509f8993f3eb19069fca371ffc875f2
kamashina <kamran.sharif@mail.ru>
2024-08-12 01:30:43 +0300

    test build kit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -14,8 +12,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./src/index.tsx"),
-      name: "SimpleUI",
-      fileName: "simple-ui",
+      name: "MossoftUI",
+      fileName: "mossoft-ui",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
