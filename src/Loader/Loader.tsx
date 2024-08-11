1daade4cb7b1f07b743995100ad5fab61b67d961
kamashina <kamran.sharif@mail.ru>
2024-08-11 22:48:18 +0300

    test

--- src/Loader/Loader.tsx
+++ src/Loader/Loader.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties, FC } from "react";
-import styles from "./loader.module.css";
+// import styles from "./loader.module.css";
 
 type Props = {
   style?: CSSProperties;
@@ -9,7 +9,7 @@ type Props = {
 const Loader: FC<Props> = ({ style, text }) => {
   return (
     <div className="flex flex-row items-center justify-center gap-3">
-      <span className={`${styles.loader} !border-b-primary`} style={style}></span>
+      <span  style={style}></span>
       {text && <span className="text-base">{text}</span>}
     </div>
   );
